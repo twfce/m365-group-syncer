eb4e98dc8d3c6bc099a62440d036c9d09e2dc5ce
twfce <me@2fce.de>
2021-11-17 20:42:03 +0100

    Initial commit

---
+++ src/build.ps1
@@ -0,0 +1,10 @@
+$requiredModules = @(
+    "Az.Storage",
+    "AzTable",
+    "Microsoft.Graph.Authentication"
+)
+
+if (-Not (Test-Path "$PSScriptRoot\modules")) { New-Item -Type Directory -Path "$PSScriptRoot\modules" }
+foreach ($module in $requiredModules) {
+    Save-Module -Path "$PSScriptRoot\modules" -Name $module
+}
