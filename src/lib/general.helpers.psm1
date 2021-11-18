723a28d77ac5c2fac76db2a39cb91324db653099
Seitle, Johannes (VWGS) <johannes.seitle@volkswagen-groupservices.com>
2021-11-18 10:09:42 +0100

    Added Prepare-Certificate function

--- src/lib/general.helpers.psm1
+++ src/lib/general.helpers.psm1
@@ -1,3 +1,43 @@
+function Prepare-Certificate {
+    Param (
+        [Parameter(ParameterSetName="CertFile")]
+        $FilePath,
+        [Parameter(ParameterSetName="KeyVaultCert")]
+        $VaultName,
+        [Parameter(ParameterSetName="KeyVaultCert")]
+        $CertificateName
+    )
+
+    begin {
+        if ($PSBoundParameters.ContainsKey("FilePath")) {
+            $certToImport = $FilePath
+        }
+        elseif ($PSBoundParameters.ContainsKey("VaultName") -and $PSBoundParameters.ContainsKey("CertificateName")) {
+            $cert = Get-AzKeyVaultCertificate -VaultName $VaultName -Name $CertificateName
+            $secret = Get-AzKeyVaultSecret -VaultName $VaultName -Name $cert.Name        
+            $secretValueText = ''
+            $ssPtr = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($secret.SecretValue)
+            try {
+                $secretValueText = [System.Runtime.InteropServices.Marshal]::PtrToStringBSTR($ssPtr)
+            } finally {
+                [System.Runtime.InteropServices.Marshal]::ZeroFreeBSTR($ssPtr)
+            }
+            $certToImport = [Convert]::FromBase64String($secretValueText) 
+        }        
+        
+        $x509Cert = new-object System.Security.Cryptography.X509Certificates.X509Certificate2
+    }
+    process {
+        if (-Not $certToImport) {
+            throw "No certificate to import"
+        }
+
+        $x509Cert.Import($certToImport, "", "Exportable,PersistKeySet")
+    }   
+    end {
+        $x509Cert
+    }
+}
 
 function Prepare-StorageTable {
     Param (
